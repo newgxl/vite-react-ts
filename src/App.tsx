204aefc5edca5559c359ee3fc3a2ee11190ecec8
GXL <1515533507@qq.com>
2023-09-23 18:35:34 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "antd"
+
+function text() {
+  console.log("hello world")
+}
+
+text()
+
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <Button type="primary">Button</Button>
+    </>
+  )
+}
+
+export default App
